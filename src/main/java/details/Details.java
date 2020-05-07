8e1f48a1ca26e3b9cf74f360d0f131c1a246d604
NishthaSharma574 <nishthasharmanamu424@gmail.com>
2020-05-07 02:59:32 -0700

    Initial Changes

---
+++ src/main/java/details/Details.java
@@ -0,0 +1,9 @@
+package details;
+
+public class Details {
+    public void print()
+    {
+        System.out.println("My name is garima");
+        System.out.println("My name is sharma");
+    }
+}
--- src/main/java/introduction/HelloWorld.java
+++ src/main/java/introduction/HelloWorld.java
@@ -1,7 +1,11 @@
 package introduction;
 
+import details.Details;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        Details obj = new Details();
+        obj.print();
     }
 }
