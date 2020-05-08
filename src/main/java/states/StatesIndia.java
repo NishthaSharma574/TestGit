5fae8acae284b5243c0f8b04de948c545acffe5e
NishthaSharma574 <nishthasharmanamu424@gmail.com>
2020-05-08 12:19:39 -0700

    Add States to Project

---
+++ src/main/java/states/StatesIndia.java
@@ -0,0 +1,11 @@
+package states;
+
+public class StatesIndia {
+    public void add()
+    {
+        System.out.println("Punjab");
+        System.out.println("Himachal");
+        System.out.println("Aurangabad");
+        System.out.println("Mumbai");
+    }
+}
